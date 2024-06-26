c1a696797801eb4c205e63071c21262062d5eafb
Andrés David López Pulido <andres.lp.dev@gmail.com>
2024-06-26 22:14:33 +0000

    test error azure

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,8 +14,6 @@ function Login() {
                     <Logo src="/img/Logo.svg" alt='LogoSena'></Logo>
                     <FormLogin/>
                 </div>
-            </div>
-        </main>
     )
 }
 
